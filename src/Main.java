1688895d8da4cae171fe82d76efb9945d24c9a6e
nourabyte <148945652+nourabyte@users.noreply.github.com>
2023-11-29 10:19:50 +0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String path = System.getProperty("user.dir");
+        
+        System.out.println(path);
+        
+        /* your code here */
+      
+    }
+}
