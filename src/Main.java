340e80c2188dc6665a6f37c66cd3589a57ed01bd
nourabyte <148945652+nourabyte@users.noreply.github.com>
2023-12-10 11:28:00 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
-
 public class Main {
     public static void main(String[] args) {
-        String path = System.getProperty("user.dir");
-        
-        System.out.println(path);
-        
-        /* your code here */
-      
+          /* try (Scanner object)) { 
+                your code here 
+           } */
     }
 }
