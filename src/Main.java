568fb36d2f96f0cadab8c88f82d0d491ef001a36
devAlq <khalidalqdev@gmail.com>
2023-12-15 18:08:36 +0300

    created scanner object

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+
+import java.util.Scanner;
+
 public class Main {
+    
     public static void main(String[] args) {
-          /* try (Scanner object)) { 
-                your code here 
-           } */
+        String name;
+        try(Scanner scanner = new Scanner(System.in)){
+            System.out.print("Please enter youre name: ");
+            name = scanner.nextLine();
+            System.out.println("welcome to this app Mr " + name);
+
+
+         }
     }
 }
